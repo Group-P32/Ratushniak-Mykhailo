3564ce352caa8a674739a888ef112b78d823047e
Ratushniak Mykhailo <rodiona07ua@gmail.com>
2026-06-07 11:28:13 +0200

    Практична 6: ViewModel, фільтрація за категорією та ціновим діапазоном

--- Ratushniak-JewelryStore/Controllers/HomeController.cs
+++ Ratushniak-JewelryStore/Controllers/HomeController.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using JewelryStore.Models;
+using System.Data.Entity;
 
 namespace Ratushniak_JewelryStore.Controllers
 {
@@ -11,11 +11,44 @@ namespace Ratushniak_JewelryStore.Controllers
     {
         JewelryContext db = new JewelryContext();
 
-        public ActionResult Index()
+        public ActionResult Index(int? categoryId, decimal? minPrice, decimal? maxPrice)
         {
-            IEnumerable<Jewelry> jewelries = db.Jewelries;
-            ViewBag.Jewelries = jewelries;
-            return View();
+            // Отримуємо всі прикраси з категоріями
+            IQueryable<Jewelry> jewelries = db.Jewelries.Include(j => j.Category);
+
+            // Фільтр за категорією
+            if (categoryId != null && categoryId != 0)
+            {
+                jewelries = jewelries.Where(j => j.CategoryId == categoryId);
+            }
+
+            // Фільтр за мінімальною ціною
+            if (minPrice != null)
+            {
+                jewelries = jewelries.Where(j => j.Price >= minPrice);
+            }
+
+            // Фільтр за максимальною ціною
+            if (maxPrice != null)
+            {
+                jewelries = jewelries.Where(j => j.Price <= maxPrice);
+            }
+
+            // Формуємо список категорій для dropdown
+            List<Category> categories = db.Categories.ToList();
+            categories.Insert(0, new Category { Name = "Всі категорії", Id = 0 });
+
+            // Формуємо ViewModel
+            JewelryListViewModel viewModel = new JewelryListViewModel
+            {
+                Jewelries = jewelries.ToList(),
+                Categories = new SelectList(categories, "Id", "Name"),
+                SelectedCategoryId = categoryId,
+                MinPrice = minPrice,
+                MaxPrice = maxPrice
+            };
+
+            return View(viewModel);
         }
 
         [HttpGet]
---
+++ Ratushniak-JewelryStore/Models/JewelryListViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace JewelryStore.Models
+{
+    public class JewelryListViewModel
+    {
+        // Список прикрас після фільтрації
+        public IEnumerable<Jewelry> Jewelries { get; set; }
+        // Список категорій для dropdown
+        public SelectList Categories { get; set; }
+        // Вибрана категорія
+        public int? SelectedCategoryId { get; set; }
+        // Мінімальна ціна
+        public decimal? MinPrice { get; set; }
+        // Максимальна ціна
+        public decimal? MaxPrice { get; set; }
+    }
+}
